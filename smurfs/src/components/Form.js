36deaf5ea4a684a54686d50ec970ab0a062a11c0
gisellehargrove <gisellehargrove@gmail.com>
2019-08-16 12:56:04 -0400

    creating form component

---
+++ smurfs/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Form = props => {
+
+};
+
+const mapStateToProps = state => {
+  return {
+
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Form);
